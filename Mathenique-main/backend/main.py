93b6520921183d3237956442a752e7f463496601
Zeruel Kody Cuenca <zeruelkody.cuenca@cvsu.edu.ph>
2026-01-15 00:07:05 +0800

    Force update for frontend

--- Mathenique-main/backend/main.py
+++ Mathenique-main/backend/main.py
@@ -16,7 +16,16 @@ app = FastAPI()
 
 app.add_middleware(
     CORSMiddleware,
-    allow_origins=["https://mathenique-frontend.onrender.com", "http://localhost:8080", "http://localhost:5173", "http://127.0.0.1:8080", "http://localhost:8082", "http://127.0.0.1:8082", "http://localhost:8083", "http://127.0.0.1:8083"], 
+    allow_origins=[
+        "https://mathenique-frontend.onrender.com",
+        "http://localhost:8080",
+        "http://localhost:5173",
+        "http://127.0.0.1:8080",
+        "http://localhost:8082",
+        "http://127.0.0.1:8082",
+        "http://localhost:8083",
+        "http://127.0.0.1:8083"
+    ], 
     allow_credentials=True,
     allow_methods=["*"],
     allow_headers=["*"],
